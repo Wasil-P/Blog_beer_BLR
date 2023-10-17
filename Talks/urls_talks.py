dac6325a250ce0c94d45f4ab7856a9c140eac1b0
Wasil P <it.dzemidok@gmail.com>
2023-10-17 13:09:29 +0200

    Change Talks templates

--- Talks/urls_talks.py
+++ Talks/urls_talks.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path("", cache_page(60)(views.CategoryTalksList.as_view()), name="categories_all"),
     path("<int:category_id>", cache_page(60)(views.OneCategoryTalks.as_view()), name="category_show_talks"),
     path("<int:category_id>/create/", views.TalkCreate.as_view(), name="talk_create"),
-    path("<int:category_id>/<int:talk_id>/edit/", views.TalkCreate.as_view(), name="talk_edit"),
+    path("<int:category_id>/<int:talk_id>/edit/", views.TalkEdit.as_view(), name="talk_edit"),
     path("<int:category_id>/<int:talk_id>", views.OneTalkShow.as_view(), name="talk_show"),
     path("<int:category_id>/<int:talk_id>/create/", views.MessageCreate.as_view(), name="message_create")
 ]
